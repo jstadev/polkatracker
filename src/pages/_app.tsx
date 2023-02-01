87f3210920b2b97f41275b3ef3fe989f7fa88627
petrankara <petroskaramitros@gmail.com>
2023-02-01 17:44:01 +0200

    next chakra tail react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,61 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
+
+export default MyApp
+
+// import { useState, useEffect } from "react"
+// import Loading from "./Loading"
+// import Profile from "./Profile"
+// import '../styles/index.css'
+
+// // const singleUser = `https://api.github.com/users/SankThomas`
+// // const repos = `https://api.github.com/users/sankthomas/repos?per_page=5`
+// // https://api.github.com/users/sankthomas/repos?page=1&per_page=10&sort=updated
+
+// function App() {
+//   const [items, setItems] = useState([])
+//   // Change this to any username whose repositories you want to get
+//   const [user] = useState("paritytech")
+
+//   useEffect(() => {
+//     const fetchRepos = async () => {
+//       const res = await fetch(
+//         `https://api.github.com/users/${user}/repos?per_page=50&sort=updated`
+//       )
+//       const data = await res.json()
+//       setItems(data)
+//       console.log(data)
+//     }
+
+//     fetchRepos()
+//   }, [user])
+
+//   return (
+//     <>
+//       <div className="pt-10">
+//         <h1 className="mb-10 font-bold text-2xl">
+//           {user} repositories
+//         </h1>
+//       </div>
+
+//       {!items ? (
+//         <Loading />
+//       ) : (
+//         <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 pb-20">
+//           {items.map((item) => (
+//             <Profile key={item.id} {...item} />
+//           ))}
+//         </div>
+//       )}
+//     </>
+//   )
+// }
+
+// export default App
